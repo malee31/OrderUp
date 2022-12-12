c62d7a44e68c78d08aea0f51184850acda57f94f
Marvin Lee <marvinlee.github@gmail.com>
2022-12-11 16:33:28 -0800

    Installed Tailwind for easier styling later on

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log(process.env)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="underline">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
