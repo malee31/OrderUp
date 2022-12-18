e6334bc6881a5e8325b17d02cac76a708f9a85b2
Marvin Lee <marvinlee.github@gmail.com>
2022-12-17 21:11:09 -0800

    Added Cart model that will promote itself into an order once placed to avoid eating up order numbers. Migrations squashed

--- django/order/models.py
+++ django/order/models.py
@@ -6,15 +6,30 @@ class MenuItem(models.Model):
     description = models.CharField(max_length=300, default="")
 
 
+class Cart(models.Model):
+    id_number = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    # Use Cart.items to access all CartItemOrder instances
+
+
 class Order(models.Model):
     order_number = models.AutoField(primary_key=True)
-    placed = models.BooleanField(null=False, blank=False, default=False)
     fulfilled = models.BooleanField(null=False, blank=False, default=False)
     # Use Order.items to access all ItemOrder instances
 
 
-class ItemOrder(models.Model):
+class ItemOrderTemplate(models.Model):
     item = models.ForeignKey(MenuItem, models.RESTRICT, null=False, blank=False)
     count = models.PositiveIntegerField(default=0, null=False, blank=False)
+
+    class Meta:
+        abstract = True
+
+
+class ItemOrder(ItemOrderTemplate):
     # TODO: Check if adding not null or blank is possible
     order = models.ForeignKey(Order, models.CASCADE, related_name="items")
+
+
+class CartItemOrder(ItemOrderTemplate):
+    # TODO: Check if adding not null or blank is possible
+    cart = models.ForeignKey(Cart, models.CASCADE, related_name="items")
