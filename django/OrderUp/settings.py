321a5b6442b58a4a60dbe06466d5d45876cfb5b1
Marvin Lee <marvinlee.github@gmail.com>
2023-01-22 01:49:31 -0800

    Reorganized Django settings.py. Now checks for 'django/build' folder before loading them in as static files or printing a warning

--- django/OrderUp/settings.py
+++ django/OrderUp/settings.py
@@ -1,36 +1,29 @@
-"""
-https://docs.djangoproject.com/en/4.1/topics/settings/
-https://docs.djangoproject.com/en/4.1/ref/settings/
-"""
 import os.path
 from os import getenv
 from pathlib import Path
-
 from dotenv import load_dotenv
 
-# Load environment variables (Env file should be ../.env)
+# Load environment variables (Env file should be in the repository root with a relative path of ../.env)
 load_dotenv()
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Set base directory for absolute path creation
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+# Django development settings
+# WARNING: Keep SECRET_KEY a secret and regenerate or make up a new one if exposed
 SECRET_KEY = getenv("SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv("DEBUG", default="False") == "True"
 
+# Django site configurations
 ALLOWED_HOSTS = getenv("ALLOWED_HOSTS", default="").split(",")
+APPEND_SLASH = False
+ROOT_URLCONF = 'OrderUp.urls'
+WSGI_APPLICATION = 'OrderUp.wsgi.application'
 
-# Application definition
+# Django app, plugins, and middleware configurations
 INSTALLED_APPS = [
     'menu',
     'cart',
     'order',
-    'build',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -50,14 +43,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-APPEND_SLASH = False
-
-ROOT_URLCONF = 'OrderUp.urls'
-
+# Template loading (For this project, the React 'django/build/index.html' is loaded in production)
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "build")],
+        'DIRS': [BASE_DIR / "build"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,22 +55,46 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+            ]
+        }
+    }
 ]
 
-WSGI_APPLICATION = 'OrderUp.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# Database configurations
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# Default primary key auto field type for implied ids
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Generated file configurations
+# Static files (For this project, it is all the React build files)
+STATIC_URL = '/react/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = []
+# Media files (For this project, it is all the optional menu item images)
+MEDIA_URL = '/mediafiles/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
+# React build overrides applied if 'django/build' exists
+REACT_BUILD_FOUND = os.path.isdir(BASE_DIR / "build")
+if not REACT_BUILD_FOUND:
+    print("Warning: No 'django/build' static folder found for STATICFILES_DIRS. Omitting React build from Django.")
+    print("^^^^^^^  This is safe to ignore in development. In production, 'build.sh' should create it")
+else:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR / "build")]
+
+# Django internationalization metadata for translations (Unused)
+# https://docs.djangoproject.com/en/4.1/topics/i18n/
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+# Other unchanged default Django configurations
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
@@ -97,24 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_TZ = True
-
-# Static files (For this project, it is all the React build files
-STATIC_URL = '/react/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "build")
-]
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Django Rest Framework Settings
 REST_FRAMEWORK = {
