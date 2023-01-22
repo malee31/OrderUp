aab7692a173682a47742638a8e356e15d4d58fdb
Marvin Lee <marvinlee.github@gmail.com>
2023-01-22 02:52:17 -0800

    S3 Django media storage set up (in theory). Testing soon

--- django/OrderUp/settings.py
+++ django/OrderUp/settings.py
@@ -8,16 +8,19 @@ load_dotenv()
 # Set base directory for absolute path creation
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Django development settings
 # WARNING: Keep SECRET_KEY a secret and regenerate or make up a new one if exposed
 SECRET_KEY = getenv("SECRET_KEY")
 DEBUG = getenv("DEBUG", default="False") == "True"
+
 
 # Django site configurations
 ALLOWED_HOSTS = getenv("ALLOWED_HOSTS", default="").split(",")
 APPEND_SLASH = False
 ROOT_URLCONF = 'OrderUp.urls'
 WSGI_APPLICATION = 'OrderUp.wsgi.application'
+
 
 # Django app, plugins, and middleware configurations
 INSTALLED_APPS = [
@@ -44,6 +47,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
 # Template loading (For this project, the React 'django/build/index.html' is loaded in production)
 TEMPLATES = [
     {
@@ -61,6 +65,7 @@ TEMPLATES = [
     }
 ]
 
+
 # Database configurations
 DATABASES = {
     'default': {
@@ -71,22 +76,45 @@ DATABASES = {
 # Default primary key auto field type for implied ids
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 # Generated file configurations
-# Static files (For this project, it is all the React build files)
+# Static files (For this project, they are all the React build files)
 STATIC_URL = '/react/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = []
-# Media files (For this project, it is all the optional menu item images)
-MEDIA_URL = '/mediafiles/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # React build overrides applied if 'django/build' exists
 REACT_BUILD_FOUND = os.path.isdir(BASE_DIR / "build")
 if not REACT_BUILD_FOUND:
     print("Warning: No 'django/build' static folder found for STATICFILES_DIRS. Omitting React build from Django.")
-    print("^^^^^^^  This is safe to ignore in development. In production, 'build.sh' should create it")
+    print("         This is safe to ignore in development, but it is expected to exist in production from 'build.sh'")
 else:
     STATICFILES_DIRS = [os.path.join(BASE_DIR / "build")]
+
+# Media files (For this project, they are all the optional menu item images)
+MEDIA_URL = '/mediafiles/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
+
+# Media files S3 bucket support
+S3_ENABLED = getenv("S3_ENABLED", "False") == "True"
+if S3_ENABLED:
+    print("Warning: Media storage in S3 buckets is enabled. Files uploaded as media in development WILL be stored")
+    # Indicate that django-storages should use S3 storage
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    # Commented out STATICFILES_STORAGE to keep React build's static files separate from S3 bucket media files
+    # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+    MEDIA_URL = getenv("S3_ACCESS_DOMAIN", getenv("S3_DOMAIN") + "/")
+
+    # S3 credentials (Prefixed with AWS, but can be from any S3-compatible storage service)
+    AWS_S3_ACCESS_KEY_ID = getenv("S3_ACCESS_KEY_ID")
+    AWS_S3_SECRET_ACCESS_KEY = getenv("S3_SECRET_ACCESS_KEY")
+    AWS_S3_REGION_NAME = getenv("S3_REGION_NAME")
+    AWS_S3_ENDPOINT_URL = getenv("S3_DOMAIN")
+    AWS_STORAGE_BUCKET_NAME = getenv("S3_STORAGE_BUCKET_NAME")
+    AWS_DEFAULT_ACL = getenv("S3_DEFAULT_ACL")
+    # TODO: Check if Cache Control is required
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=43200'}  # 12 Hours
+
 
 # Django internationalization metadata for translations (Unused)
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
@@ -94,6 +122,7 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
+
 
 # Other unchanged default Django configurations
 # Password validation
